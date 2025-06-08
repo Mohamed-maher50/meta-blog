b6e5fad9c6fce8f8e67ec574941f479b5e0c5183
mohamedmaher <mohamedmaher.mm330@gmail.com>
2025-06-08 12:08:44 +0200

    create view all Articles button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
 import ArticleCard from "@/components/ArticleCard";
 import Container from "@/components/Container";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className=" font-[family-name:var(--font-work-sans)]">
       <Container>
-        <div className="grid gap-5 grid-cols-3">
-          <ArticleCard />
-          <ArticleCard />
-          <ArticleCard />
+        <div className="flex flex-col gap-y-8">
+          <div className="grid gap-5 grid-cols-3">
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+          </div>
+          <Button
+            className="mx-auto block text-secondary-foreground"
+            variant={"outline"}
+          >
+            View All Articles
+          </Button>
         </div>
       </Container>
     </div>
