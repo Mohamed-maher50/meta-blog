f731de2351108b210dca5b6ed488b7ce94d97d45
mohamedmaher <mohamedmaher.mm330@gmail.com>
2025-06-23 12:14:33 +0300

    fetch blogs from api and display it

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,34 @@
 import ArticleCard from "@/components/ArticleCard";
 import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
-// import { auth } from "@/lib/auth";
+import { AuthorBlog } from "@/types";
 
 export default async function Home() {
   // const session = await auth();
+  const res = await fetch("http://localhost:3000/api/blogs?omit[]=content", {
+    // cache: "force-cache",
+  });
+  if (!res.ok) throw new Error(`can't fetch blogs`);
+  const blogs: AuthorBlog[] = await res.json();
+  console.log(blogs);
+  console.log(blogs);
+
   return (
     <main className=" font-[family-name:var(--font-work-sans)]">
       <Container>
+        <div className=" h-72">
+          {/* <Image
+            src={blogs[0].images[0].url}
+            width={1920}
+            height={1080}
+            alt=""
+          /> */}
+        </div>
         <div className="flex flex-col gap-y-8">
           <div className="grid gap-5 sm:grid-cols-2 md:grid-cols-3">
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
+            {blogs.map((b) => {
+              return <ArticleCard {...b} key={b.id} />;
+            })}
           </div>
           <Button
             className="mx-auto block text-secondary-foreground"
