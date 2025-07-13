ebcd8be0625a593b5ff73f5f0c239cbf9b13498d
mohamedmaher <mohamedmaher.mm330@gmail.com>
2025-07-13 14:37:13 +0300

    add forwardRef for this component

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { Input } from "./ui/input";
 import { Search } from "lucide-react";
-const SearchInput = () => {
-  return (
-    <div className="relative">
-      <Input
-        type="search"
-        className="min-w-40 placeholder:text-secondary-foreground-400 font-normal text-sm"
-        placeholder="Search"
-      />
-      <Search className=" absolute w-4 h-4  top-0 translate-y-1/2 origin-center right-2  pointer-events-none text-secondary-foreground-600   " />
-    </div>
-  );
-};
+
+type SearchInputProps = React.ComponentPropsWithoutRef<typeof Input>;
+
+const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
+  (props, ref) => {
+    return (
+      <div className="relative">
+        <Input
+          ref={ref}
+          type="search"
+          className="min-w-40 placeholder:text-secondary-foreground-400 font-normal text-sm"
+          placeholder="Search"
+          {...props}
+        />
+        <Search className=" absolute w-4 h-4  top-0 translate-y-1/2 origin-center right-2  pointer-events-none text-secondary-foreground-600   " />
+      </div>
+    );
+  }
+);
+
+SearchInput.displayName = "SearchInput";
 
 export default SearchInput;
