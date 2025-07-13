7e7e08486e65d8e47b828eb0e5cf3a96430616c5
mohamedmaher <mohamedmaher.mm330@gmail.com>
2025-07-13 15:39:14 +0300

    make card as link without wrap whole content good for seo and intractive

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -9,42 +9,65 @@ import {
 import Image from "next/image";
 import { Badge } from "./ui/badge";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { TArticle } from "@/types";
+import { AuthorBlog } from "@/types";
 import { longDate } from "@/lib/utils";
+import Link from "next/link";
 
-const ArticleCard: FC<TArticle> = () => {
+const ArticleCard: FC<AuthorBlog> = ({
+  title,
+  author,
+  images,
+  id,
+  category,
+}) => {
+  const coverImage = images?.find((img) => {
+    return img.type == "cover";
+  });
   return (
-    <article>
-      <Card>
-        <CardHeader>
-          <Image
-            src={"https://picsum.photos/250/300"}
-            width={360}
-            height={240}
-            className="object-cover w-full   sm:w-96 max-h-60 h-60   rounded-sm"
-            alt="article 1 "
-          />
-        </CardHeader>
+    <Card className="h-full relative isolate">
+      <Link href={`/blogs/${id}`}>
+        <span className="absolute  z-10 inset-0"></span>
+      </Link>
+      <CardHeader>
+        <Image
+          src={coverImage?.secure_url || ""}
+          width={800}
+          height={450}
+          className="object-cover w-full  mx-auto  sm:w-96 max-h-60 h-60   rounded-sm"
+          alt="article 1 "
+        />
+      </CardHeader>
 
-        <CardContent>
-          <Badge className="bg-primary/5 text-primary">Technology</Badge>
-          <CardTitle className="text-secondary-foreground-800 dark:text-white">
-            The Impact of Technology on the Workplace: How Technology is
-            Changing
-          </CardTitle>
-        </CardContent>
-        <CardFooter className="flex text-secondary-foreground-400">
-          <div className="flex gap-2 justify-center  items-center">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <span className="font-medium">mohamed maher</span>
-            <span>{longDate(1717843200000)}</span>
-          </div>
-        </CardFooter>
-      </Card>
-    </article>
+      <CardContent>
+        <Badge className="bg-primary/5 text-primary">
+          {category.name || "Category"}
+        </Badge>
+        <CardTitle className="text-secondary-foreground-800 line-clamp-2 dark:text-white">
+          {title ||
+            "The Impact of Technology on the Workplace: How Technology is Changing"}
+        </CardTitle>
+      </CardContent>
+
+      <CardFooter className="flex mt-auto text-secondary-foreground-400">
+        <div className="flex gap-2  justify-center  items-center">
+          <Link
+            className="relative flex justify-center items-center gap-2 z-20"
+            href={`/author/${author.id}`}
+          >
+            {author?.image && (
+              <Avatar>
+                {<AvatarImage src={author.image?.url} />}
+
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            )}
+
+            <span className="font-medium ">{author?.name}</span>
+          </Link>
+          <span>{longDate(1717843200000)}</span>
+        </div>
+      </CardFooter>
+    </Card>
   );
 };
 
